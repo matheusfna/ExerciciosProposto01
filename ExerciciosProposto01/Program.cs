905fb08ce526da98aab8c5be2c89a436d64d6373
matheusfna <matheus_guga@hotmail.com>
2025-03-10 11:32:00 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosProposto01/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExerciciosProposto01 {
+    class URI {
+        static void Main(string[] args) {
+
+            int n;
+
+            n = int.Parse(Console.ReadLine());
+
+            double[] vet = new double[n];
+
+            string[] valor = Console.ReadLine().Split(' ');
+
+
+            for(int i = 0; i < n; i++) {
+                vet[i] = double.Parse(valor[i],CultureInfo.InvariantCulture);
+            }
+
+            double maior = vet[0];
+
+            int posicaoMaior = 0;
+
+            for(int i = 1; i < n; i++) {
+                if(vet[i] > maior) {
+                    maior = vet[i];
+                    posicaoMaior = i;
+                }
+            }
+
+            Console.WriteLine(maior.ToString("F1",CultureInfo.InvariantCulture));
+            Console.WriteLine(posicaoMaior);
+        }
+    }
+}
+
+
